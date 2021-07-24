8300d548410006696d57c777686672ee414775eb
Saigo1997 <a.saigo1997@gmail.com>
2021-07-24 20:07:54 +0900

    fixes #6 GoでRESTサーバー＋ファイルシステムアクセスのアプリを作ってみる

--- backend/server.go
+++ backend/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 
 	"github.com/labstack/echo"
@@ -8,8 +9,19 @@ import (
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "uzume server")
-	})
+	e.GET("/loadfile", loadfile)
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func loadfile(c echo.Context) error {
+	bytes, err := ioutil.ReadFile("./response.txt")
+	if err != nil {
+		return c.String(http.StatusOK, "error")
+	}
+
+	dat := map[string]string{
+		"dat": string(bytes),
+	}
+
+	return c.JSON(http.StatusOK, dat)
 }
