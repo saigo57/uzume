d76a8a35b4d8bb3acbbc7a2c62ac3c3145c63012
Saigo1997 <a.saigo1997@gmail.com>
2021-12-30 16:13:23 +0900

    fixes #133 タググループとタグのツリー表示を左エリアに表示する

--- frontend/src/view/mainMenu.tsx
+++ frontend/src/view/mainMenu.tsx
@@ -1,18 +1,27 @@
 import React, { useState, useEffect} from 'react';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome, faStar, faQuestion, faTag } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faStar, faQuestion, faTag, faFolder, faCaretRight, faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import { faCircle } from "@fortawesome/free-regular-svg-icons";
+import { useTags } from './lib/tagCustomHooks';
 
 type MainMenuProps = {
+  workspaceId: string
   workspaceName: string
   currMode: string
   onAction: (action: string) => void
+  onSingleTagClick: (tagId: string) => void
   dsb_ref: React.RefObject<HTMLElement>
 };
 
 export const MainMenu:React.VFC<MainMenuProps> = (props) => {
   const [FavoriteTagListState, setFavoriteTagList] = useState(['タグ1', 'タグ2', 'タグ3', 'タグ4']);
+  const [tagGroupListState, tagAllListState, _showingTagAllListState, _resetTagList, _selectingMenu, _selectMenu] = useTags(props.workspaceId);
+  const [isTagGroupOpen, setIsTagGroupOpen] = useState({} as { [key: string]: boolean });
+
+  useEffect(() => {
+    setIsTagGroupOpen({})
+  }, [tagGroupListState]);
 
   const onHomeClick = () => {
     if ( props.onAction ) props.onAction('home_click');
@@ -29,6 +38,14 @@ export const MainMenu:React.VFC<MainMenuProps> = (props) => {
   const selectedClass = (menu: string): string => {
     return props.currMode == menu ? 'selected' : '';
   };
+
+  const onTagGroupClick = (tagGroupId: string) => {
+    setIsTagGroupOpen((state) => {
+      var next_state = {...state}
+      next_state[tagGroupId] = !next_state[tagGroupId]
+      return next_state
+    })
+  }
 
   return (
     <section id="main-menu" className="main-menu" ref={props.dsb_ref}>
@@ -52,6 +69,34 @@ export const MainMenu:React.VFC<MainMenuProps> = (props) => {
           FavoriteTagListState.map((tag) => {
             return <li className="tag-item"><FontAwesomeIcon icon={faCircle} />{tag}</li>
           }) 
+        }
+      </ul>
+      <h1 className="menu-title">
+        <FontAwesomeIcon icon={faFolder} /><div className="menu-title-text">タググループ</div>
+      </h1>
+      <ul>
+        {
+          tagGroupListState.map((tag_group) => {
+            return (
+              <>
+                <li className="tag-group-item" onClick={ () => { onTagGroupClick(tag_group.tagGroupId) } } >
+                  <FontAwesomeIcon icon={ isTagGroupOpen[tag_group.tagGroupId] ? faCaretDown : faCaretRight }/>
+                  {tag_group.name}
+                </li>
+                {(() => {
+                  // タグ
+                  if ( isTagGroupOpen[tag_group.tagGroupId] ) {
+                    return (
+                      tagAllListState.map((tag) => {
+                        if ( tag.tagGroupId != tag_group.tagGroupId ) return;
+                        return <li className="tag-item" onClick={() => { props.onSingleTagClick(tag.tagId) }}><FontAwesomeIcon icon={faTag} />{tag.name}</li>
+                      })
+                    )
+                  }
+                })()}
+              </>
+            )
+          })
         }
       </ul>
     </section>
