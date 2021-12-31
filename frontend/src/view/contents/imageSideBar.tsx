49140247d7abf3a6ba1dda0dfa6843d519d4fedd
Saigo1997 <a.saigo1997@gmail.com>
2021-12-31 22:59:17 +0900

    fixes #131 タグ付与のUIを調整する

--- frontend/src/view/contents/imageSideBar.tsx
+++ frontend/src/view/contents/imageSideBar.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect} from 'react';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { useTags } from '../lib/tagCustomHooks';
 import {
   IpcId as ImageIpcId,
   RequestImageInfo,
@@ -8,10 +7,7 @@ import {
   RemoveTagFromImage,
 } from '../../ipc/images'
 import {
-  IpcId as TagsIpcId,
-  GetAllTags,
   TagInfo,
-  TagList,
 } from '../../ipc/tags'
 import { Tag } from "../component/atmos/tag";
 import { TagCtrlPanel } from "../component/organisms/tagCtrlPanel";
@@ -26,9 +22,9 @@ type ImageSideBarProps = {
 };
 
 export const ImageSideBar:React.VFC<ImageSideBarProps> = (props) => {
-  const [tagListState, setTagList] = useState([] as TagInfo[]);
   const [imageInfoList, setImageInfoList] = useState([] as ImageInfo[]);
   const [isShowTagCtrlPanel, setIsShowTagCtrlPanel] = useState(false);
+  const [_tagGroupListState, tagListState, _showingTagAllListState, _resetTagList, _selectingMenu, _selectMenu] = useTags(props.workspaceId);
 
   useEffect(() => {
     updateImageInfo();
@@ -45,13 +41,6 @@ export const ImageSideBar:React.VFC<ImageSideBarProps> = (props) => {
   useEffect(() => {
     sendIpcGetAllTags(props.workspaceId)
   }, [props.workspaceId]);
-
-  useEffect(() => {
-    window.api.on(TagsIpcId.GET_ALL_TAGS_REPLY, (_e, arg) => {
-      const tagList = JSON.parse(arg) as TagList
-      setTagList(tagList.tags)
-    });
-  }, []);
 
   useEffect(() => {
     window.api.on(ImageIpcId.IMAGE_INFO_LIST_UPDATED_REPLY, (_e, arg) => {
@@ -121,72 +110,74 @@ export const ImageSideBar:React.VFC<ImageSideBarProps> = (props) => {
   if ( imageInfoList.length == 1 ) showImageInfo = imageInfoList[0];
 
   return (
-    <section id="image-side-bar" className="image-side-bar" ref={props.dsb_ref}>
-      <div className="side-bar-title"><div className="side-bar-title-text">Tag</div></div>
-      <div className="tag-area" onClick={onTagAreaClick}>
-        <div className="tagged-area">
-          { linkedTagList.map((t) => {
-              return (
-                <Tag
-                  workspaceId={props.workspaceId}
-                  tagId={t.tagId}
-                  tagName={t.name}
-                  favorite={t.favorite}
-                  delete={true}
-                  alreadyAdded={false}
-                  onClick={null}
-                  onDeleteClick={removeTag}
-                />
-              )
-            })
-          }
+    <>
+      <section id="image-side-bar" className="image-side-bar" ref={props.dsb_ref}>
+        <div className="side-bar-title"><div className="side-bar-title-text">Tag</div></div>
+        <div className="tag-area" onClick={onTagAreaClick}>
+          <div className="tagged-area">
+            { linkedTagList.map((t) => {
+                return (
+                  <Tag
+                    workspaceId={props.workspaceId}
+                    tagId={t.tagId}
+                    tagName={t.name}
+                    favorite={t.favorite}
+                    delete={true}
+                    alreadyAdded={false}
+                    onClick={null}
+                    onDeleteClick={removeTag}
+                  />
+                )
+              })
+            }
+          </div>
         </div>
-      </div>
+        <div className="side-bar-title"><div className="side-bar-title-text">Info</div></div>
+        <ul className="info-area">
+          <li className="info-item">
+            <div className="info-title">メモ</div>
+            <textarea name="memo" className="info-body editable memo" value="画像についてのメモを記述する。編集はreactのstateを介して行う必要があるよう。" />
+          </li>
+          <li className="info-item">
+            <div className="info-title">作者</div>
+            <input type="text" name="author" className="info-body editable" value="ジャムおじさん" />
+          </li>
+          <div className="info-item-block-separator"></div>
+          <li className="info-item">
+            <div className="info-title">ファイル名</div>
+            <div className="info-body freezed">{showImageInfo.file_name}</div>
+          </li>
+          <li className="info-item">
+            <div className="info-title">UUID</div>
+            <div className="info-body freezed">{showImageInfo.image_id}</div>
+          </li>
+          <li className="info-item">
+            <div className="info-title">形式</div>
+            <div className="info-body freezed">{showImageInfo.ext}</div>
+          </li>
+          <li className="info-item">
+            <div className="info-title">登録日</div>
+            <div className="info-body freezed">{showImageInfo.created_at}</div>
+          </li>
+          <li className="info-item">
+            <div className="info-title">解像度</div>
+            <div className="info-body freezed">{showImageInfo.width} × {showImageInfo.height}</div>
+          </li>
+          <li className="info-item">
+            <div className="info-title">容量</div>
+            <div className="info-body freezed">2.43MB</div>
+          </li>
+        </ul>
+      </section>
+
       <TagCtrlPanel
         workspaceId={props.workspaceId}
         display={isShowTagCtrlPanel}
         imageIds={props.imageIds}
-        tagInfoList={tagListState}
         alreadyLinkedTag={linkedTagList}
         onRemoveTag={removeTag}
         onClose={() => { setIsShowTagCtrlPanel(false) }}
       />
-      <div className="side-bar-title"><div className="side-bar-title-text">Info</div></div>
-      <ul className="info-area">
-        <li className="info-item">
-          <div className="info-title">メモ</div>
-          <textarea name="memo" className="info-body editable memo" value="画像についてのメモを記述する。編集はreactのstateを介して行う必要があるよう。" />
-        </li>
-        <li className="info-item">
-          <div className="info-title">作者</div>
-          <input type="text" name="author" className="info-body editable" value="ジャムおじさん" />
-        </li>
-        <div className="info-item-block-separator"></div>
-        <li className="info-item">
-          <div className="info-title">ファイル名</div>
-          <div className="info-body freezed">{showImageInfo.file_name}</div>
-        </li>
-        <li className="info-item">
-          <div className="info-title">UUID</div>
-          <div className="info-body freezed">{showImageInfo.image_id}</div>
-        </li>
-        <li className="info-item">
-          <div className="info-title">形式</div>
-          <div className="info-body freezed">{showImageInfo.ext}</div>
-        </li>
-        <li className="info-item">
-          <div className="info-title">登録日</div>
-          <div className="info-body freezed">{showImageInfo.created_at}</div>
-        </li>
-        <li className="info-item">
-          <div className="info-title">解像度</div>
-          <div className="info-body freezed">{showImageInfo.width} × {showImageInfo.height}</div>
-        </li>
-        <li className="info-item">
-          <div className="info-title">容量</div>
-          <div className="info-body freezed">2.43MB</div>
-        </li>
-      </ul>
-    </section>
+    </>
   );
 }
